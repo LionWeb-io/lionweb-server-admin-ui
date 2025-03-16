be08a6b1859ea4e5490d2fa677060ab9a8f7fc89
ftomassetti <federico@strumenta.com>
2025-03-16 15:20:29 +0100

    Removing unused types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,30 +1,3 @@
-import type { SerializationChunk } from '@lionweb/core';
-
-export interface Repository {
-	repository_name: string;
-	schema_name: string;
-	history: boolean;
-	lionweb_version: string;
-	created?: string;
-}
-
-export interface RepositoryListResponse {
-	success: boolean;
-	repositories: Repository[];
-	messages: Array<{
-		kind: string;
-		message: string;
-	}>;
-}
-
-export interface CreateRepositoryRequest {
-	name: string;
-	description: string;
-	owner: string;
-	version: string;
-	languages: string[];
-}
-
 export interface Partition {
 	id: string;
 }
@@ -32,13 +5,4 @@ export interface Partition {
 export interface PartitionListResponse {
 	partitions: Partition[];
 	total: number;
-}
-
-export interface BulkListPartitionsResponse {
-	partitionIds: string[];
-	total: number;
-}
-
-export interface CreatePartitionRequest {
-	chunk: SerializationChunk;
 }
